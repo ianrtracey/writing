3828ce747a5d74e005651a87c8928e5d8ed4eff5
Ian Tracey <itracey@pinterest.com>
2019-09-18 00:07:24 -0700

    more editing

--- src/pages/death-dinner.js
+++ src/pages/death-dinner.js
@@ -71,8 +71,8 @@ export default class DeathDinner extends React.Component {
         <div>
           <Title text="Host Your Own Death Dinner">
             <Subheading>
-              This serves as a basic guide and outline for hosting your own
-              death dinner. Adapted from the guides found at
+              This serves as a basic guide for hosting your own death dinner.
+              Adapted from the guides found at
               <OutboundLink
                 href={"https://deathoverdinner.org"}
                 style={{
@@ -122,21 +122,19 @@ export default class DeathDinner extends React.Component {
               table and acknowledge those who have gone on before us. Let’s do a
               quick around the table introduction, to be completed with a “raise
               of the glass” to someone who is no longer with us, who impacted
-              your life in some way or you admire deeply. In no more than twenty
-              words, share why you admire them. If you would prefer to light a
-              candle in their honor, we have found that to be meaningful as
-              well, some people do both.
+              your life in some way or who you admire deeply. In no more than
+              twenty words, share why you admire them. After sharing, light your
+              candle.
             </Text>
           </Section>
 
           <Section text="Phase II - Exploring Death">
             <Text>
               We’ll now go around the table, allowing everyone time to answer.
-              Each person will describe their experience and follow up questions
-              can be made but no sharing of personal anecdotes until it’s your
-              turn.
+              Follow up questions can be asked by anyone at the table but no
+              sharing of personal anecdotes until it’s your turn.
             </Text>
-            <Text>Ask the following questions (or a subset): </Text>
+            <Text>Some questions to get the conversation started: </Text>
             <Question>For what do you want to be remembered? </Question>
             <Question>
               What would you want your final 30 days to look like?
@@ -156,19 +154,14 @@ export default class DeathDinner extends React.Component {
           <Section text="Phase III - Your Death">
             <Text>
               We’ll now go around the table, allowing everyone time to answer.
-              Each person will describe their experience and follow up questions
-              can be made but no sharing of personal anecdotes until it’s your
-              turn.
+              Follow up questions can be asked by anyone at the table but no
+              sharing of personal anecdotes until it’s your turn.
             </Text>
-            <Question>How you want your own life to end?</Question>
+            <Question>How do you want your own life to end?</Question>
             <Question>
               Imagine you had a crystal ball and could watch the future day of
               your death, what do you think you would see, hour by hour until
               your death?
-            </Question>
-            <Question>
-              How might you feel during your final hours? Minutes? Who is around
-              you?
             </Question>
             <Question>
               How you would like to support the end-of-life wishes of those you
@@ -178,37 +171,30 @@ export default class DeathDinner extends React.Component {
 
           <Section text="Phase IV - Silence">
             <Text>
-              To end the dinner, we will conduct “the Pause” and a round of
-              appreciation. “The Pause” is a shared moment to remember ourselves
-              inside the expanse of our existence, of our consciousness and the
-              love in our life.
+              As we draw to a close, we will conduct “the Pause” followed by a
+              round of appreciation. “The Pause” is a shared moment to take
+              notice of ourselves inside the expanse of our existence, our
+              consciousness, the love in our life and the ones we've lost.
             </Text>
-            <Text>
-              Here is a suggested script to introduce the pause: “Let us take a
-              moment to Pause and honor all the persons we have cared for who
-              died. Each of them was someone who loved and was loved; someone’s
-              family member, friend or lover.
-            </Text>
-            <Question>Take a 1-2 minutes moment of silence</Question>
+            <Question>Take 1-2 minutes for a moment of silence</Question>
           </Section>
 
           <Section text="Phase V - Appreciation">
             <Text>
-              The round of appreciation completes the conversation and the
-              dinner. Someone begins by selecting a person on their left and
-              right, and sharing one thing they admire about that person. The
-              person who is appreciated can only say, “thank you”, and the
-              circle continues until everyone has been admired once. If you just
-              met the person beside you tonight, find something during the
-              dinner that stuck you about them that you can reflect back to
+              To close the evening, someone begins by sharing one thing they
+              admire about the person to their left and the person to their
+              right. The person who is appreciated can only say, “Thank you".
+              The circle continues until everyone has been admired once. If you
+              just met the person beside you tonight, find something during the
+              dinner that stuck out about them that you can reflect back to
               them.
             </Text>
             <Text>
               We suggest that after the appreciation you break from the table
-              and do whatever calls you, whether you look up at the vastness of
-              the sky or lounge on a couch to digest it all, anything that
-              further sinks you into the connections and awareness that you
-              experienced at the table. Tonight is your night.
+              and do whatever calls you. Whether you look up at the vastness of
+              the sky or lounge on a couch to digest it all, do something that
+              further deepens connections and awareness that you experienced at
+              the table. Tonight is your night.
             </Text>
           </Section>
         </div>
